8ae1b9b15f0db5b66690421a48066c44aa373ace
reorreo <reocci@gmail.com>
2024-05-03 17:46:30 +0900

    test

--- DNN/deep.py
+++ DNN/deep.py
@@ -25,9 +25,6 @@ def test(net, loader):
     min_index_list=[]
     targes_list=[]
     images_list=[]
-    predict = []
-    correct = 0
-    total = 0
     with torch.no_grad():
         for batch_idx, (img_path, inputs, targets, labels) in enumerate(loader):
             # DNNの予測
@@ -43,6 +40,7 @@ def test(net, loader):
             # バッチ内で最も予測確率が高いクラスの中で，
             # 最も予測確率が低いサンプルのインデックスを取得し，リストとして保存
             min_index_list.append(int(min_index[predicted]))
+            # 
             targes_list.append(targets)
             images_list.append(img_path)
 
@@ -52,6 +50,19 @@ def test(net, loader):
 def create_model():
     model = models.resnet50(pretrained=True)
     return model 
+
+# model_name = {
+#     'ResNet50': [models.resnet50(pretrained=True)],
+#     'VGG16': [models.alexnet('resnet18', cifar=True)],
+#     'bigresnet50': [resnet.ResNet('resnet18', cifar=True), 2048],
+#     'bigresnet18_preact': [preact_resnet.ResNet18, 512],
+#     'resnet18': [resnet.ResNet('resnet18'), 512],
+#     'resnet34': [resnet.ResNet('resnet34'), 512],
+#     'resnet50': [resnet.ResNet('resnet50'), 2048],
+#     # 追加
+#     'Coresnet50': [resnet.ResNet('resnet50', preact=True), 2048],
+# }
+
 # データローダーをインスタンス化
 loader = test_dataloader(root=dir_path,batch_size=batch_size,  num_class=class_num, random_numbers=random_numbers)
 test_loader = loader.run()
